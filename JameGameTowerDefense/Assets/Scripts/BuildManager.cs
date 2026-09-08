3c77aeaea034d4b87e180261e233ef134f4ddc20
Max Schroeder <max.schroeder@mymail.champlain.edu>
2026-09-08 17:03:14 -0500

    Adding some ui Assets and making a hotbar for build mode

--- JameGameTowerDefense/Assets/Scripts/BuildManager.cs
+++ JameGameTowerDefense/Assets/Scripts/BuildManager.cs
@@ -5,6 +5,8 @@ public class BuildManager : MonoBehaviour
 {
     public InputAction BuildModeAction;
     public InputAction PlaceBuildAction;
+    public InputAction ChangeSelectedBuildAction;
+    public InputAction ScrollChangeSelectedBuildAction;
 
     public bool InBuildMode = false;
     public int currentTowerIndex = 0;
@@ -17,31 +19,42 @@ public class BuildManager : MonoBehaviour
     private GameObject OutlineObject;
     private int floorMask;
 
+    private Animator currentHotbar;
+
     private void OnEnable()
     {
         BuildModeAction.Enable();
         PlaceBuildAction.Enable();
+        ChangeSelectedBuildAction.Enable();
+        ScrollChangeSelectedBuildAction.Enable();
     }
     private void OnDisable()
     {
         BuildModeAction.Disable();
         PlaceBuildAction.Disable();
+        ChangeSelectedBuildAction.Disable();
+        ScrollChangeSelectedBuildAction.Disable();
     }
     private void Start()
     {
         BuildModeAction.performed += context => SwitchBuildMode();
         PlaceBuildAction.performed += context => PlaceBuild();
+        ChangeSelectedBuildAction.performed += context => KeyChangedHotbar(context.control.name);
+        ScrollChangeSelectedBuildAction.performed += context => ScrollChangedHotbar(context.ReadValue<Vector2>().y);
+
         mainCamera = Camera.main.gameObject;
         floorMask = LayerMask.GetMask("Floor");
 
-        SetOutlineObject(TowerOutlines[currentTowerIndex]);
+        SetOutlineObject(currentTowerIndex);
+        InBuildMode = false;
         OutlineObject.SetActive(InBuildMode);
         BuildUI.SetActive(InBuildMode);
+        
     }
-    private void SetOutlineObject(GameObject newOutlineObject)
+    private void SetOutlineObject(int target)
     {
         if (OutlineObject != null) Destroy(OutlineObject);
-        OutlineObject = Instantiate(TowerOutlines[currentTowerIndex]);
+        OutlineObject = Instantiate(TowerOutlines[target]);
     }
     private void SwitchBuildMode()
     {
@@ -49,6 +62,58 @@ public class BuildManager : MonoBehaviour
         BuildUI.SetActive(InBuildMode);
         OutlineObject.SetActive(InBuildMode);
         OutlineObject.transform.position = RaycastToFloor();
+        UpdateHotbar(currentTowerIndex + 1);
+    }
+    private void KeyChangedHotbar(string key)
+    {
+        if (!InBuildMode) return;
+        if(int.TryParse(key, out int num))
+        {
+            ChangeHotBarTarget(num-1);
+        }
+    }
+    private void ScrollChangedHotbar(float delta)
+    {
+        if (!InBuildMode) return;
+        Debug.Log(delta);
+        if (delta > 0)
+        {
+            currentTowerIndex++;
+            if(currentTowerIndex >= TowerOutlines.Length)
+            {
+                currentTowerIndex = 0;
+            }
+        }
+        else
+        {
+            currentTowerIndex--;
+            if(currentTowerIndex < 0)
+            {
+                currentTowerIndex = TowerOutlines.Length - 1;
+            }
+        }
+        ChangeHotBarTarget(currentTowerIndex);
+    }
+    private void ChangeHotBarTarget(int target)
+    {
+        if (target < 0 || target >= TowerOutlines.Length) return;
+        currentTowerIndex = target;
+        SetOutlineObject(currentTowerIndex);
+        UpdateHotbar(currentTowerIndex + 1);
+    }
+    private void UpdateHotbar(int newTarget)
+    {
+        if(currentHotbar != null)
+        {
+            currentHotbar.SetTrigger("PlayDeselect");
+        }
+        GameObject newHotBar = GameObject.Find($"HotbarItem{newTarget}");
+        if(newHotBar != null)
+        {
+            Animator animator = newHotBar.GetComponentInChildren<Animator>();
+            animator.SetTrigger("PlaySelect");
+            currentHotbar = animator;
+        }
     }
     private void PlaceBuild()
     {
@@ -67,7 +132,6 @@ public class BuildManager : MonoBehaviour
         mainCamera = Camera.main.gameObject;
         if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward), out RaycastHit hit, 1000f, floorMask))
         {
-            Debug.Log(hit.transform.position);
             return hit.point;
         }
 
