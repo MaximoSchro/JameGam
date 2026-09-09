658d086d839b3718382056b17d1552f3cc995813
vctr <victorpdiab@gmail.com>
2026-09-10 01:47:21 +0300

    Work on Enemy and spline
    Enemy Movement, Health, and spline evaluations WIP.

---
+++ JameGameTowerDefense/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField]  private int maxHealth;
+    private int _currentHealth;
+
+    private void Start()
+    {
+        ResetHealth();
+    }
+
+    private void ResetHealth()
+    {
+        _currentHealth = maxHealth;
+    }
+
+    internal void TakeDamage(int damage = 1)
+    {
+        _currentHealth -= damage;
+
+        if (damage <= 0)
+        {
+            HandleDeath();
+        }
+    }
+
+    private void HandleDeath()
+    {
+        //change this bullshit :P
+        Destroy(gameObject);
+    }
+}
