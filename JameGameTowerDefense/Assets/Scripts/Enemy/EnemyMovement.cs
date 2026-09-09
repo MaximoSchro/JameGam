2d04fc1f2a0bc6a5e49548f65f1942c4a707eb89
vctr <victorpdiab@gmail.com>
2026-09-10 02:23:21 +0300

    Enemy Movement Changes
    Stun and Slow down created.

--- JameGameTowerDefense/Assets/Scripts/Enemy/EnemyMovement.cs
+++ JameGameTowerDefense/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,26 +1,64 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Splines;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 10;
+    [SerializeField] private float speed = 10f;
     
-    private SplineContainer spline;
-    private float dist;
+    [SerializeField] float slowPerStack = 0.1f;
+    [SerializeField] private float maxSlowDown = 0.3f;
+    
+    private SplineContainer _spline;
+    private float _dist;
+    private bool _isStunned = false;
+    
+    private int _slowStacks = 0;
     
     void Start()
     {
         //will change this to a singleton spline if i care later
-        spline = GameObject.Find("Spline").GetComponent<SplineContainer>();
-        dist = 0;
+        _spline = GameObject.Find("Spline").GetComponent<SplineContainer>();
+        _dist = 0;
     }
 
     void Update()
     {
-        dist += Time.deltaTime * speed;
-        SplineFunctions.SplineEvaluate(spline, dist, out Vector3 pos, out float t);
+        //clean up this line if it upsets anyone
+        _dist += !_isStunned ? Time.deltaTime * speed * Mathf.Clamp((1f - slowPerStack * _slowStacks), maxSlowDown, 1f) : 0f;
+        SplineFunctions.SplineEvaluate(_spline, _dist, out Vector3 pos, out float t);
         
         transform.position = pos;
         if (t >= 1) Destroy(gameObject); //do whatever losing thing is supposed to happen when they get to the end
+    }
+
+    void Stun(float time)
+    {
+        StartCoroutine("ApplyStun", time);
+    }
+
+    IEnumerator ApplyStun(float time)
+    {
+        _isStunned = true;
+        yield return new WaitForSeconds(time);
+        _isStunned = false;
+    }
+
+    void AddSlowStacks(int amount = 1)
+    {
+        _slowStacks += amount;
+    }
+    
+    void RemoveSlowStacks(int amount = 1)
+    {
+        _slowStacks -= amount;
+        _slowStacks = _slowStacks < 0 ? 0 : _slowStacks;
+    }
+    
+    void ClearSlowStacks()
+    {
+        _slowStacks = 0;
     }
 }
