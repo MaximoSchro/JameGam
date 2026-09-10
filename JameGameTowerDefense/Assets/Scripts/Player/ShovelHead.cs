dec21bfbc819e725bdd2f2a81dcfeb7f5bfd6836
Max Schroeder <max.schroeder@mymail.champlain.edu>
2026-09-10 16:55:28 -0500

    Making different tiers of swings unique

--- JameGameTowerDefense/Assets/Scripts/Player/ShovelHead.cs
+++ JameGameTowerDefense/Assets/Scripts/Player/ShovelHead.cs
@@ -1,14 +1,42 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ShovelHead : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController;
+
+    [SerializeField] private float boxHalfWidth;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Enemy" && !playerController.CheckAnimationName("ShovelSwing")) return;
-        int currentCharge = playerController.GetChargeState();
         Vector3 cameraForward = Camera.main.transform.forward;
-        Rigidbody rb = other.GetComponent<Rigidbody>();
-        rb.AddForceAtPosition(cameraForward * 100 * Mathf.Pow(5, currentCharge), other.ClosestPoint(transform.position));
+        int currentCharge = playerController.GetChargeState();
+        Rigidbody rb;
+        switch (currentCharge)
+        {
+            case 1:
+                //do stun
+                break;
+            case 2:
+                    rb = other.GetComponent<Rigidbody>();
+                    rb.AddForceAtPosition(cameraForward * 100 * Mathf.Pow(5, currentCharge), other.ClosestPoint(transform.position));
+                break;
+            case 3:
+                RaycastHit[] hits = Physics.BoxCastAll(this.transform.position + this.transform.forward * 5, Vector3.one * boxHalfWidth, this.transform.forward);
+                foreach(RaycastHit hit in hits)
+                {
+                    if(hit.transform.gameObject.tag != "Enemy") continue;
+                    rb = hit.transform.GetComponent<Rigidbody>();
+                    rb.AddForceAtPosition(cameraForward * 100 * Mathf.Pow(5, currentCharge), other.ClosestPoint(transform.position));
+                }
+                break;
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(this.transform.position + Camera.main.transform.forward * 2 +Vector3.up*2, Vector3.one * boxHalfWidth);
     }
 }
